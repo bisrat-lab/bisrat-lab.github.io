6396d0846026bf7e4c2d00a37a1d699846bbc2a8
Bisrat Tegegne <wow.bisrat11@gmail.com>
2021-04-15 22:29:38 -0400

    Update script.js

--- lab-12/script.js
+++ lab-12/script.js
@@ -28,3 +28,32 @@ class Clock {
         this.timer = setInterval(()=> this.render(),1000);
     }
 }
+
+//class Student {
+constructor(firstName, lastName, grades = []) {
+this.firstName = firstName;
+this.lastName = lastName;
+this.grades = grades;
+}
+inputNewGrade(newGrade) {
+this.grades.push(newGrade);
+}
+computeAverageGrade() {
+return this.grades.reduce((sum, current, index, array) => sum + current /
+array.length, 0);
+}
+}
+const stu1 = new Student('John', 'Smith');
+stu1.inputNewGrade(88);
+stu1.inputNewGrade(98);
+stu1.inputNewGrade(86);
+stu1.inputNewGrade(80);
+const stu2 = new Student('John2', 'Smith2');
+stu2.inputNewGrade(85);
+stu2.inputNewGrade(95);
+stu2.inputNewGrade(85);
+stu2.inputNewGrade(70);
+const students = [stu1, stu2];
+const result = students.reduce((average, stu, index, array) => average + stu.comp
+uteAverageGrade() / array.length, 0);
+console.log(result);
